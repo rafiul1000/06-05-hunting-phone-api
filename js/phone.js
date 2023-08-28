184f3526d6d7ec392bcc39fcccad8d592b273420
rafiul1000 <rafiulislam5075@gmail.com>
2023-08-28 12:10:39 +0600

    modal first part added

--- js/phone.js
+++ js/phone.js
@@ -45,8 +45,8 @@ console.log(phones.length)
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+            <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show details</button>
             </div>
         </div>
     `;
@@ -58,6 +58,15 @@ console.log(phones.length)
   toggleLoadingSpinner(false);
 
 };
+
+// 
+const handleShowDetail = async (id) =>{
+  console.log('clicked show details', id);
+  // load single phone data
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data = await res.json();
+  console.log(data);
+}
 
 // handle search button
 const handleSearch = (isShowAll) =>{
